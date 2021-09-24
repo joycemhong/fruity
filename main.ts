26b7026ce19a40df907f5597fd9e0004942051fe
joycemhong <91283949+joycemhong@users.noreply.github.com>
2021-09-24 10:40:33 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+scene.setBackgroundColor(9)
+let fish = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . . . 
+    . . . c 4 d 4 4 4 4 4 1 c . c c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+    . . f f 4 d 4 4 f f 4 c f c . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(fish)
+fish.setStayInScreen(true)
+info.setScore(0)
+info.startCountdown(30)
+game.onUpdateInterval(500, function () {
+	
+})
